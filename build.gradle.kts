896eb8e7077165a4d263119625c759d080452d30
please-give-me-a-coke <pleasegivemeacok@gmail.com>
2025-10-16 23:20:35 +0900

    feat: Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.graalvm.buildtools.native") version "0.11.1"
+}
+
+group = "com.please-give-me-a-coke"
+version = "0.0.1-SNAPSHOT"
+description = "Cheapest parcel project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/com/please_give_me_a_coke/cheapest_parcel/CheapestParcelApplication.java
@@ -0,0 +1,13 @@
+package com.please_give_me_a_coke.cheapest_parcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapestParcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheapestParcelApplication.class, args);
+	}
+
+}
